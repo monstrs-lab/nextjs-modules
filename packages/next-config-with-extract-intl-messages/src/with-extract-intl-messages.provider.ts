75c7f6c449efdea13a8250045d73fa3e9bd1a71a
Andrey Linko <AndreyLinko@gmail.com>
2020-07-22 14:40:20 +0300

    feat(common): extract config per env

--- packages/next-config-with-extract-intl-messages/src/with-extract-intl-messages.provider.ts
+++ packages/next-config-with-extract-intl-messages/src/with-extract-intl-messages.provider.ts
@@ -15,28 +15,28 @@ export const withExtractIntlMessages = (nextConfig: any = {}) => {
   return {
     ...nextConfig,
     webpack(config, options) {
-      if (!options.isServer) {
-        config.module.rules.forEach((rule) => {
-          if (rule.use && rule.use.loader === 'next-babel-loader') {
-            // eslint-disable-next-line
-            rule.use.options.plugins = [
-              ...(rule.use.options.plugins || []),
-              require.resolve('./babel-plugin-resolve-message-scope'),
-              [require.resolve('babel-plugin-react-intl'), reactIntlPluginOptions],
-            ]
-          }
-        })
+      config.module.rules.forEach((rule) => {
+        if (rule.use && rule.use.loader === 'next-babel-loader') {
+          // eslint-disable-next-line
+          rule.use.options.plugins = [
+            ...(rule.use.options.plugins || []),
+            require.resolve('./babel-plugin-resolve-message-scope'),
+            [require.resolve('babel-plugin-react-intl'), reactIntlPluginOptions],
+          ]
+        }
+      })
 
-        // eslint-disable-next-line global-require
-        const { CombineMessagesPlugin } = require('./combine-messages.webpack-plugin')
+      // eslint-disable-next-line global-require
+      const { CombineMessagesPlugin } = require('./combine-messages.webpack-plugin')
 
-        config.plugins.push(
-          new CombineMessagesPlugin(
-            reactIntlPluginOptions.messagesDir,
-            path.join(reactIntlPluginOptions.messagesDir, 'messages.json')
-          )
+      const prefix = options.isServer ? 'server' : 'client'
+
+      config.plugins.push(
+        new CombineMessagesPlugin(
+          reactIntlPluginOptions.messagesDir,
+          path.join(reactIntlPluginOptions.messagesDir, `messages-${prefix}.json`)
         )
-      }
+      )
 
       if (typeof nextConfig.webpack === 'function') {
         return nextConfig.webpack(config, options)
