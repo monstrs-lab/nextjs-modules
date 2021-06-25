ec50437d71a3191cc76fd361980759a540db87be
Andrey Linko <AndreyLinko@gmail.com>
2021-06-25 12:50:36 +0300

    feat(common): pnp workspaces next loader resolution

--- packages/next-document-with-viewport/src/with-viewport.provider.tsx
+++ packages/next-document-with-viewport/src/with-viewport.provider.tsx
@@ -4,18 +4,20 @@ import React    from 'react'
 const defaultViewport =
   'width=device-width,minimum-scale=1,maximum-scale=1,initial-scale=1,user-scalable=no'
 
-export const withViewport = (viewport = defaultViewport) => (TargetComponent) =>
-  class WithViewport extends TargetComponent {
-    static async getInitialProps(context) {
-      const props = await super.getInitialProps(context)
+export const withViewport =
+  (viewport = defaultViewport) =>
+  (TargetComponent) =>
+    class WithViewport extends TargetComponent {
+      static async getInitialProps(context) {
+        const props = await super.getInitialProps(context)
 
-      props.head.push(<meta name='viewport' content={viewport} />)
+        props.head.push(<meta name='viewport' content={viewport} />)
 
-      return props
+        return props
+      }
+
+      static renderDocument(...args) {
+        // @ts-ignore
+        return Document.renderDocument(...args)
+      }
     }
-
-    static renderDocument(...args) {
-      // @ts-ignore
-      return Document.renderDocument(...args)
-    }
-  }
